3da68c6dd1b6714bd9011e71c8d597ddcd5a2fb3
Nick Jackson <nickdjackson@msn.com>
2022-10-31 16:59:00 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'; //have to call Hooks inside of the component
 import './App.css';
+import SingleCoffee from './SingleCoffee';
 
 function App() {
+  const [data, setData] = useState([{name: 'Colada', price: '40cents'}]) //first item is the getter (gets the data). Second item is the setter - changes the data.
+
+  
+  
+  useEffect(() => {   //use effect runs when the component is doing. If the array is empty
+    getCoffee()
+  }, [])
+
+
+  const getCoffee = () => {
+    fetch("https://api.sampleapis.com/coffee/hot")
+    .then(response=>response.json()) //each .then and .catch takes a callback function. This line converts to json.
+    .then((data) => setData(data)) // get converted data
+    .catch(x => console.error(x))
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>Coffees by Sample API</h1>
+        <button onClick={() => getCoffee()}>Get Me Coffees</button>
+        <div className='menu-items'>
+          {data.map((item) => {
+            console.log('each item', item)
+            return <SingleCoffee entireObject={item}/>
+            })}
+            </div>
+        </main>
     </div>
   );
 }
---
+++ src/SingleCoffee.js
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+export default function SingleCoffee ({entireObject}) { // take out name, price, description - destructuring the props object. In essence we are creating variables out of the properties of the whole props object.
+    const [favorite, setFavorite] = useState(false)
+    // Example of destructuring below 
+    //   const exampleProps = {
+    //     description: "Black cup of coffee",
+    //     name: "Cafe Cubano",
+    //     price: "Mucho Mula"
+    // }
+
+    // const {description, name, price} = exampleProps 
+
+
+    // console.log('props here->', name, price, description) // just to test we console.log. Props is the entire object
+    return ( //inside of the return, onyl JSX is allowed (except for in curly braces)
+        <div className="menu-items_item" style={{backgroundColor: favorite ? 'red' : 'white'}}>
+        <img 
+        src={entireObject.image}
+        alt="Black coffee in a white porcelain cup" 
+        />
+        <div>
+          <h3>{entireObject.name}</h3> 
+          <span>Price: {entireObject.price}</span>
+          <p>{entireObject.description}</p>
+          <button onClick={() => setFavorite(true)}>Favorite</button>
+          {/* each single coffee gets to keep its own state */}
+        </div>
+      </div>
+    )
+}
